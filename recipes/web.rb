b82e81a352e57a80a3eb67ccb99f81b2c7afe790
Marius Sturm <marius@torch.sh>
2014-07-11 13:06:01 +0200

    restart webui

--- recipes/web.rb
+++ recipes/web.rb
@@ -13,19 +13,19 @@ template "/etc/graylog2/web/graylog2-web-interface.conf" do
   source "graylog2.web.conf.erb"
   owner 'root'
   mode 0644
-  notifies :restart, 'service[graylog2-server]'
+  notifies :restart, 'service[graylog2-web]'
 end
 
 template "/etc/default/graylog2-web" do
   source "graylog2.web.default.erb"
   owner 'root'
   mode 0644
-  notifies :restart, 'service[graylog2-server]'
+  notifies :restart, 'service[graylog2-web]'
 end
 
 template "/etc/graylog2/web/logback.xml" do
   source "graylog2.web.logback.xml.erb"
   owner 'root'
   mode 0644
-  notifies :restart, 'service[graylog2-server]'
+  notifies :restart, 'service[graylog2-web]'
 end
