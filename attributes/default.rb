0c9fd4ba343cdd88c46232ab3e4a3dd1954ca628
Bernd Ahlers <bernd@torch.sh>
2014-08-01 15:06:21 +0200

    Fix typo in key name.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ node.default[:mongodb]  ||= {}
 # General
 default.graylog2[:server][:version] = '0.20.6-1'
 default.graylog2[:web][:version]    = '0.20.6-1'
-default.graylog2[:radio][:versioo]  = '0.20.6-1'
+default.graylog2[:radio][:version]  = '0.20.6-1'
 default.graylog2[:user]             = 'graylog2'
 default.graylog2[:group]            = 'graylog2'
 default.graylog2[:root_username]    = 'admin'
