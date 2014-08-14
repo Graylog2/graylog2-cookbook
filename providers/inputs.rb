b8b37ea09c0720141c77d54973dedb86a8ffa4ce
Marius Sturm <marius@torch.sh>
2014-08-14 17:56:44 +0200

    make this idempotent

--- providers/inputs.rb
+++ providers/inputs.rb
@@ -1,3 +1,4 @@
+require 'json'
 require 'faraday'
 
 action :create do
@@ -7,14 +8,39 @@ action :create do
     faraday.basic_auth(node[:graylog2][:rest][:admin_access_token], 'token')
     faraday.adapter(Faraday.default_adapter)
   end
-  
+ 
   if new_resource.input
-    response = connection.post('/system/inputs', resource_input, { :'Content-Type' => 'application/json' })
-    Chef::Log.debug("Graylog2 API response: #{response.status}")
-  elsif not node[:graylog2][:inputs].nil?
-    node[:graylog2][:inputs].each do |input|
+    inputs = Array.new(resource_input)
+  else
+    inputs = node[:graylog2][:inputs]
+  end
+
+  if not inputs.nil?
+    inputs.each do |input|
+      parsed_input = JSON.parse(input)
+    
+      response = connection.get('/system/inputs')
+      if !response.body.nil?
+        parsed_response = JSON.parse(response.body)
+        saved_inputs = parsed_response.fetch("inputs")
+        if existent_input?(saved_inputs, parsed_input)
+          break
+        end
+      end
+
       response = connection.post('/system/inputs', input, { :'Content-Type' => 'application/json' })
       Chef::Log.debug("Graylog2 API response: #{response.status}")
     end
   end
+end
+
+def existent_input?(saved_inputs, input)
+  saved_inputs.each do |saved_input|
+    message_input = saved_input.fetch("message_input")
+    if message_input.fetch("title") == input.fetch("title")
+      return true
+    end
+  end
+
+  return false
 end
