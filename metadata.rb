0a6b34bd8b9c51283d7c3af48e61131aec6d3127
Marius Sturm <marius@graylog.com>
2015-03-18 21:52:00 +0100

    add maintainer

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ name             "graylog2"
 maintainer       "Graylog, Inc."
 maintainer_email "marius@graylog.com"
 license          "Apache 2.0"
-description      "Installs and configures Graylog"
+description      "Installs and configures Graylog - maintained by Graylog, Inc."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.3"
 
