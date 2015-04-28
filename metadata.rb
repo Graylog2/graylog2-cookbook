c85198c242f773c7312ea866dbaf7acaa51a2d44
Tomas Brazys <tomas.brazys@gmail.com>
2015-04-28 17:02:34 +0300

    Add configuration for stream alerts

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email "marius@graylog.com"
 license          "Apache 2.0"
 description      "Installs and configures Graylog - maintained by Graylog, Inc."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.3"
+version          "1.0.4"
 
 depends 'authbind', '>= 0.1.8'
 
--- providers/streams.rb
+++ providers/streams.rb
@@ -11,7 +11,7 @@ action :create do
     faraday.adapter(Faraday.default_adapter)
     faraday.use :repeater, retries: 5, mode: :one
   end
- 
+
   if new_resource.stream
     streams = [new_resource.stream]
   else
@@ -43,10 +43,36 @@ action :create do
       end
       Chef::Application.fatal!("You need to set stream rules in order to create a stream") if rules.nil?
 
+      if new_resource.alert_conditions
+        alert_conditions = JSON.parse(new_resource.alert_conditions)
+      else
+        alert_conditions = parsed_stream['alert_conditions']
+        parsed_stream.delete('alert_conditions')
+      end
+
+      if alert_conditions
+        if new_resource.alarm_callbacks
+          alarm_callbacks = JSON.parse(new_resource.alarm_callbacks)
+        else
+          alarm_callbacks = parsed_stream['alarm_callbacks']
+          parsed_stream.delete('alarm_callbacks')
+        end
+      end
+
       stream_id = create_stream(connection, parsed_stream)
 
       rules.each do |rule|
         create_stream_rule(connection, stream_id, rule)
+      end
+
+      if alert_conditions
+        alarm_callbacks.each do |alarm_callback|
+          create_stream_alarm_callback(connection, stream_id, alarm_callback)
+        end
+
+        alert_conditions.each do |alert_condition|
+          create_stream_alert_condition(connection, stream_id, alert_condition)
+        end
       end
 
       resume_stream(connection, stream_id)
@@ -62,7 +88,7 @@ def create_stream(connection, data)
   rescue Exception => e
     Chef::Application.fatal!("Failed to create stream #{data.fetch('title')}.\n#{e.message}")
   end
-    
+
   return stream_id
 end
 
@@ -71,6 +97,16 @@ def create_stream_rule(connection, stream_id, data)
   Chef::Log.info("Graylog2 API response: #{response.status}")
 
   return response
+end
+
+def create_stream_alarm_callback(connection, stream_id, data)
+  response = connection.post("/streams/#{stream_id}/alarmcallbacks", data.to_json, { :'Content-Type' => 'application/json' })
+  Chef::Log.info("Graylog2 API response: #{response.status}")
+end
+
+def create_stream_alert_condition(connection, stream_id, data)
+  response = connection.post("/streams/#{stream_id}/alerts/conditions", data.to_json, { :'Content-Type' => 'application/json' })
+  Chef::Log.info("Graylog2 API response: #{response.status}")
 end
 
 def resume_stream(connection, stream_id)
