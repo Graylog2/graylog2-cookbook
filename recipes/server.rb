8d8e41d828bf39de32e31c94db47bb1295a428e8
Marius Sturm <marius@graylog.com>
2015-11-02 19:06:34 +0100

    satisfy rubocop

--- recipes/server.rb
+++ recipes/server.rb
@@ -15,9 +15,7 @@ if node.graylog2[:elasticsearch][:unicast_search_query] && node.graylog2[:elasti
   node.set[:graylog2][:elasticsearch][:discovery_zen_ping_unicast_hosts] = nodes.map { |ip| ip + ':9300' }.join(',')
 end
 
-if node.graylog2[:server][:install_tzdata_java]
-  package 'tzdata-java'
-end
+package 'tzdata-java' if node.graylog2[:server][:install_tzdata_java]
 
 package 'graylog-server' do
   action :install
