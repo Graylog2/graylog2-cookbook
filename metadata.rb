58ab786e7a56ed1671eecad64c383375d53ce577
Marius Sturm <marius@graylog.com>
2016-01-04 11:35:49 +0100

    bump cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'marius@graylog.com'
 license 'Apache 2.0'
 description 'Installs and configures Graylog - maintained by Graylog, Inc.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.2.3'
+version '1.3.2'
 
 depends 'authbind', '>= 0.1.8'
 depends 'java'
