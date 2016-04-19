a3f13ad77499d4f20556e49e46ce7e9581d654db
Justin Nauman <justin.r.nauman+github@gmail.com>
2016-04-19 13:11:40 -0500

    Setup default secrets hash
    Fixes #55

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,4 +1,5 @@
 # Override attributes from data bag's "server" section
+secrets = {}
 begin
   secrets = Chef::EncryptedDataBagItem.load(node.graylog2[:secrets_data_bag], 'graylog')['server']
 rescue
