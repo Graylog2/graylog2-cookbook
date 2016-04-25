71aeae642758240a7e4353469e8807732a2a5d31
Marius Sturm <marius@graylog.com>
2016-04-25 15:28:28 +0200

    cleanup Foodcritic and Rubocop

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,8 @@
 name 'graylog2'
 maintainer 'Graylog, Inc.'
 maintainer_email 'marius@graylog.com'
+source_url 'https://github.com/Graylog2/graylog2-cookbook'
+issues_url 'https://github.com/Graylog2/graylog2-cookbook/issues'
 license 'Apache 2.0'
 description 'Installs and configures Graylog - maintained by Graylog, Inc.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
