ff5b35098f944bdb9ec84c3dc641e9d6dc29d2b0
Andrey Aleksandrov <postgred@gmail.com>
2017-09-27 18:31:22 +0300

    use api_helper for auth settings
    Signed-off-by: Andrey Aleksandrov <postgred@gmail.com>

--- resources/check_api.rb
+++ resources/check_api.rb
@@ -1,20 +1,19 @@
-property :uri, String, name_property: true
-property :token, String
+property :api_uri, String, nil], name_property: true
+property :api_token, [String, nil], default: nil
 
 default_action :check
 
 action :check do
   require 'graylogapi'
+  extend Extensions::ApiHelper
 
   log 'Wait until the Graylog2 API is ready' do
     level :info
   end
 
-  url = new_resource.uri || node['graylog2']['rest']['listen_uri']
-  token = new_resource.token || node['graylog2']['rest']['admin_access_token']
   retries = node['graylog2']['api_client_timeout'] || 300
 
-  graylogapi = GraylogAPI.new(base_url: url, token: token)
+  graylogapi = GraylogAPI.new(auth_params(new_resource))
 
   begin
     graylogapi.client.request(:get, '/')
