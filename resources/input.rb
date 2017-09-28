4d105262aee0a08bec566e2b1dfcfef27721e720
Andrey Aleksandrov <postgred@gmail.com>
2017-09-28 18:34:35 +0300

    property for all parameters
    Signed-off-by: Andrey Aleksandrov <postgred@gmail.com>

--- resources/input.rb
+++ resources/input.rb
@@ -1,8 +1,7 @@
 property :title, String, name_property: true, required: true
 property :hostname, [String, nil], default: nil
 property :type, String, required: true
-property :address, String, required: true
-property :port, Integer, required: true
+property :settings, Hash, required: true
 
 # api auth settings
 property :api_uri, [String, nil], default: nil
@@ -20,11 +19,7 @@ action :create do
     title: new_resource.title,
     global: new_resource.hostname.nil?,
     node: new_resource.hostname.nil? ? nil : new_resource.hostname,
-    configuration: {
-      bind_address: new_resource.address,
-      port: new_resource.port
-    }
-  }
+  }.merge(new_resource.settings)
 
   options[:type] =
     begin
--- test/fixtures/cookbooks/api_resources_test/recipes/default.rb
+++ test/fixtures/cookbooks/api_resources_test/recipes/default.rb
@@ -1,12 +1,10 @@
 graylog2_input 'global beats input' do
   type 'Beats'
-  address '0.0.0.0'
-  port 5014
+  settings(configuration: { bind_address: '0.0.0.0', port: 5044 })
 end
 
 graylog2_input 'local syslog tcp input' do
   hostname 'f594fbd6-9277-4e8f-9eba-e8d318f41872'
   type 'Syslog TCP'
-  address '0.0.0.0'
-  port 5015
+  settings(configuration: { bind_address: '0.0.0.0', port: 5015 })
 end
