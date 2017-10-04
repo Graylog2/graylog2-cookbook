91bf37bae25f63bdc6926c6ec2c010d2b4ba4944
Andrey Aleksandrov <postgred@gmail.com>
2017-10-05 00:00:15 +0300

    create and update resource only if need
    Signed-off-by: Andrey Aleksandrov <postgred@gmail.com>

--- resources/input.rb
+++ resources/input.rb
@@ -9,25 +9,53 @@ property :api_token, [String, nil], default: nil
 
 default_action :create
 
-action :create do
+# this field use for update request
+attr_reader :input_id
+
+load_current_value do
   require 'graylogapi'
   extend Extensions::ApiHelper
+  graylogapi = GraylogAPI.new(auth_params(self))
 
-  graylogapi = GraylogAPI.new(auth_params(new_resource))
+  inputs = graylogapi.system.inputs.all['inputs']
+  current_input = inputs.find { |i| i['title'] == title }
 
-  options = {
-    title: new_resource.title,
-    global: new_resource.hostname.nil?,
-    node: hostname_to_node_id(graylogapi, new_resource.hostname),
-    type: type_to_type_id(graylogapi, new_resource.type),
-  }.merge(new_resource.settings)
+  current_value_does_not_exist! if current_input.nil?
 
-  response = graylogapi.system.inputs.create(options)
+  title current_input['title']
+  type type_id_to_type(graylogapi, current_input['type'])
+  hostname current_input['global'] ? nil : node_id_to_hostname(graylogapi, current_input['node'])
+  settings(configuration: keys_to_symbols(current_input['attributes']))
 
-  log 'Can`t create input' do
-    message response.body.to_s
-    level :fatal
-    only_if { response.fail? }
+  # this use for update request
+  @input_id = current_input['id']
+end
+
+action :create do
+  converge_if_changed do
+    require 'graylogapi'
+    extend Extensions::ApiHelper
+    graylogapi = GraylogAPI.new(auth_params(new_resource))
+
+    options = {
+      title: new_resource.title,
+      global: new_resource.hostname.nil?,
+      node: hostname_to_node_id(graylogapi, new_resource.hostname),
+      type: type_to_type_id(graylogapi, new_resource.type),
+    }.merge(new_resource.settings)
+
+    response =
+      if current_resource.nil?
+        graylogapi.system.inputs.create(options)
+      else
+        graylogapi.system.inputs.update(current_resource.input_id, options)
+      end
+
+    log 'Can`t create input' do
+      message response.body.to_s
+      level :fatal
+      only_if { response.fail? }
+    end
   end
 end
 
@@ -43,6 +71,25 @@ def hostname_to_node_id(graylogapi, hostname)
   node_id['node_id']
 end
 
+def node_id_to_hostname(graylogapi, node_id)
+  graylogapi.system.cluster.nodes['nodes'].find do |node|
+    node['node_id'] == node_id
+  end['hostname']
+end
+
 def type_to_type_id(graylogapi, type)
   graylogapi.system.inputs.types.name_to_type(type)
+end
+
+def type_id_to_type(graylogapi, type_id)
+  graylogapi.system.inputs.types.all.body.find do |_, type|
+    type['type'].casecmp(type_id).zero?
+  end.last['name']
+end
+
+def keys_to_symbols(hash)
+  hash.each_with_object({}) do |(k, v), new_hash|
+    new_hash[k.to_sym] = v
+    new_hash
+  end
 end
