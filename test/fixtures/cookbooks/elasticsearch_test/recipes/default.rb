6b6ed0b3245f288e488c3af6d5ef9d414b09a665
Donald Morton <donald.morton@graylog.com>
2021-08-06 17:24:14 -0500

    Override deprecated JVM options in Elasticsearch recipe.

--- test/fixtures/cookbooks/elasticsearch_test/recipes/default.rb
+++ test/fixtures/cookbooks/elasticsearch_test/recipes/default.rb
@@ -8,6 +8,21 @@ elasticsearch_install 'elasticsearch' do
 end
 elasticsearch_configure 'elasticsearch' do
   allocated_memory '512m'
+  jvm_options %w(
+                -XX:+AlwaysPreTouch
+                -server
+                -Xss1m
+                -Djava.awt.headless=true
+                -Dfile.encoding=UTF-8
+                -Djna.nosys=true
+                -XX:-OmitStackTraceInFastThrow
+                -Dio.netty.noUnsafe=true
+                -Dio.netty.noKeySetOptimization=true
+                -Dio.netty.recycler.maxCapacityPerThread=0
+                -Dlog4j.shutdownHookEnabled=false
+                -Dlog4j2.disable.jmx=true
+                -XX:+HeapDumpOnOutOfMemoryError
+              )
   configuration ({
     'cluster.name' => node['elasticsearch']['cluster']['name']
   })
