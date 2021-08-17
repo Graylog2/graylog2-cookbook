6ae7ebfed43de802d76e9daa23e7455a87242b83
Donald Morton <donald.morton@graylog.com>
2021-08-17 14:25:40 -0500

    Update sidecar name in integration tests.

--- test/integration/openjdk/serverspec/sidecar_spec.rb
+++ test/integration/openjdk/serverspec/sidecar_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe service('collector-sidecar') do
+describe service('graylog-sidecar') do
   it { should be_enabled }
   it { should be_running }
 end
 
-describe file('/etc/graylog/collector-sidecar/collector_sidecar.yml') do
+describe file('/etc/graylog/sidecar/sidecar.yml') do
   it { should be_file }
   its(:content) { should match(%r{server_url: http:\/\/localhost:9000\/api}) }
 end
