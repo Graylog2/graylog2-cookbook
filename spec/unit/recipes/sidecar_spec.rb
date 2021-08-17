53e541a93eac36c4299bf8a72b2850e95b1b8cfd
Donald Morton <donald.morton@graylog.com>
2021-08-17 10:13:15 -0500

    Fix sidecar rspec. Update recipe version to 4.0.0.

--- spec/unit/recipes/sidecar_spec.rb
+++ spec/unit/recipes/sidecar_spec.rb
@@ -1,36 +1,36 @@
 require 'spec_helper'
 
-describe 'graylog2::collector_sidecar' do
-  context 'when the cookbook installs collector-sidecar on a Ubuntu system' do
+describe 'graylog2::sidecar' do
+  context 'when the cookbook installs graylog-sidecar on a Ubuntu system' do
     let(:chef_run) do
       ChefSpec::ServerRunner.new(
         platform: 'ubuntu',
         version: '20.04',
         file_cache_path: '/tmp'
       ) do |node|
-        node.normal['graylog2']['sidecar']['version'] = '0.0.8'
-        node.normal['graylog2']['sidecar']['build'] = 1
-      end.converge('graylog2::collector_sidecar')
+        node.normal['graylog2']['sidecar']['version'] = '1.1.0'
+        node.normal['graylog2']['sidecar']['release'] = 1
+      end.converge('graylog2::sidecar')
     end
 
-    it 'downloads the sidecar package' do
-      expect(chef_run).to create_remote_file_if_missing '/tmp/collector-sidecar_0.0.8-1_amd64.deb'
+    it 'downloads the sidecar repository package' do
+      expect(chef_run).to create_remote_file_if_missing '/tmp/graylog-sidecar-repository_1-2_all.deb'
     end
 
     it 'installs the sidecar package' do
-      expect(chef_run).to install_package 'graylog-collector-sidecar'
+      expect(chef_run).to install_apt_package('graylog-sidecar')
     end
 
     it 'installs a system service' do
-      expect(chef_run).to run_execute('/usr/bin/graylog-collector-sidecar -service install')
+      expect(chef_run).to run_execute('/usr/bin/graylog-sidecar -service install')
     end
 
     it 'restarts sidecar service' do
-      expect(chef_run).to enable_service 'collector-sidecar'
+      expect(chef_run).to enable_service 'graylog-sidecar'
     end
 
     it 'creates a sidecar configuration file' do
-      expect(chef_run).to render_file('/etc/graylog/collector-sidecar/collector_sidecar.yml')
+      expect(chef_run).to render_file('/etc/graylog/sidecar/sidecar.yml')
     end
   end
 end
